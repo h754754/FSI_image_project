f9ee7ec3ea8476d85fd95d3f4ebeec32a1c484a8
LopCuns <jorgelopezcuns@gmail.com>
2026-05-10 19:19:25 +0200

    bugs from last update

--- Task6_preview.m
+++ Task6_preview.m
@@ -1,5 +1,8 @@
 %Existent sample characters are g.bmp, r.bmp, a.bmp and s.bmp
+% imageBinarization works with texts with a white background and a black
+% text, but these sample images have withe text in black background, hence
+% the use of not()
 readChar = charPreprocessing(not(imageBinarization(im2double(imread("g.bmp")))));
-matchedChar = matchCharacter(readChar,tempx);
+matchedChar = matchCharacter(readChar,charDB);
 disp(["the estimated character is --->", matchedChar])
 
